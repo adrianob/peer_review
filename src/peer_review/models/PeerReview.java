cf885659d9b63a83d0f2ebc31296730ad5594aad
gallegretti <gabriel13082@hotmail.com>
2016-06-03 21:34:16 -0300

    Create PeerReview class

---
+++ src/peer_review/models/PeerReview.java
@@ -0,0 +1,10 @@
+package peer_review.models;
+import peer_review.data.*;
+public class PeerReview {
+	public UserInterface ui;
+	public Database db;
+	
+	public static void main(String [] args) {
+
+	}
+}
